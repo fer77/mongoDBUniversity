8950b2135ab22680237e6c1994c0ded0a50b5d13
Fernando Fernandez <ffmp777@yahoo.com>
2016-04-20 14:11:02 -0600

    Chapter 2 notes & homework

---
+++ hwchapter2/student.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  courses: [{ type: String, ref: 'Course' }]
+});
+
+/* Returns the student's first name, which we will define
+ * to be everything up to the first space in the student's name.
+ * For instance, "William Bruce Bailey" -> "William" */
+schema.virtual('firstName').get(function() {
+  var firstName = this.name;
+  return firstName.substr(0, firstName.search(' '));
+});
+
+/* Returns the student's last name, which we will define
+ * to be everything after the last space in the student's name.
+ * For instance, "William Bruce Bailey" -> "Bailey" */
+schema.virtual('lastName').get(function() {
+  var lastName = this.name.split(' ');
+  return lastName[lastName.length - 1];
+});
+
+module.exports = schema;
